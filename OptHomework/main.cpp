9d6d9022d4a5eaa3956c27bf07ca7fbd71030970
Gergely Tarsoly <gergely.tarsoly3@gmail.com>
2019-03-03 17:01:56 +0100

    Modify Optional Homework

--- OptHomework/main.cpp
+++ OptHomework/main.cpp
@@ -1,22 +1,59 @@
 #include <cassert>
 
+#include "SmartPointer.hpp"
+
+class A{
+public:
+    A() { ++alive; };
+    ~A() { --alive; };
+    static int alive;
+};
+
+int A::alive = 0;
+
 int main()
 {
-    Stack<double> ds;
-    assert(ds.size() == 0);
+    /*{
+        SmartPointer<int> p;
+        *p = 7;
+        assert(*p == 7);
+        assert(p.getCounter() == 1);
+        
+        const SmartPointer<int> q{p};
+        assert(p.getCounter() == q.getCounter());
+        assert(p.getCounter() == 2);
 
-    ds.push(12).push(14).push(54);
-    assert(ds.size() == 3);
-    
-    ds.add();
-    assert(ds.pop() == 68);
+        {
+            SmartPointer<int> scoped{p};
+            assert(scoped.getCounter() == 3);
+            assert(*scoped == 7);
+        }
 
-    ds.push(60);
-    assert(ds.pop() == 60);
-    assert(ds.size() == 1);
-    
-    ds.clear();
-    assert(ds.size() == 0);
+        assert(p.getCounter() == 2);
+    }*/
+    /*{
+        {
+            SmartPointer<A> a;
+            assert(A::alive == 1);
+            SmartPointer<A> b{a};
+
+            assert(A::alive == 1);
+            assert(a.getCounter() == b.getCounter());
+            assert(a.getCounter() == 2);
+        }
+        assert(A::alive == 0);
+    }*/
+    /*{
+        {
+            SmartPointer<A> a;
+            assert(A::alive == 1);
+            SmartPointer<A> b;
+            assert(A::alive == 2);
+            b = a;
+            assert(A::alive == 1);
+        }
+        assert(A::alive == 0);
+    }*/
 
     return 0;
 }
